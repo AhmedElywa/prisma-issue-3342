6acb4b97c1f5e3a0040796563eeaa327d45f9c25
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-08-20 14:59:31 +0200

    add not null example

--- main.ts
+++ main.ts
@@ -3,21 +3,12 @@ import { PrismaClient } from '@prisma/client';
 async function main() {
   const prisma = new PrismaClient();
   const result = await prisma.buyer.findMany({
-    select: {
-      sales: {
-        where: {
-          OR: [
-            {
-              date: '2020-08-19T10:02:43.353Z', // not working
-              // date: { equals: '2020-08-19T10:02:43.353Z' }, working
-            },
-          ],
-        },
-      },
+    where: {
+      name: { not: null },
     },
   });
   console.log(result);
-  prisma.disconnect();
+  prisma.$disconnect();
 }
 
 main();
